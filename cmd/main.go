d931108b1ab20db7acf0bb79f935ca2a0f06473a
Michel Diz <MichelDiz@users.noreply.github.com>
2025-03-17 14:40:05 -0300

    Update launch configuration and logging path; add initial test file

--- cmd/main.go
+++ cmd/main.go
@@ -22,7 +22,7 @@ var (
 
 func init() {
 	// Setup file logging
-	logFile, err := os.OpenFile("dql-lsp.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	logFile, err := os.OpenFile("./testing/dql-lsp.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatal("Failed to open log file:", err)
 	}
